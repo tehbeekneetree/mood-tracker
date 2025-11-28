45d161fadca107840d8067a6b54dd7d42710ef52
tehbeeknee <158133252+tehbeekneetree@users.noreply.github.com>
2025-11-29 00:15:45 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
 let dateDisplay = document.getElementById("date-display");
+let saveButton = document.getElementById("save-btn");
+
+let happinessValue = null;
+let angerValue = null;
+
 let date = new Date();
 
 function intToDate(num) {
@@ -32,8 +37,25 @@ function setDate() {
     let dateDay = date.getDate();
     let month = date.getMonth();
     let year = date.getFullYear();
-    console.log(`${weekDay}, ${dateDay}.${month}.${year}`);
     dateDisplay.innerHTML = `${weekDay}, ${dateDay}.${month}.${year}`
 }
 
-window.addEventListener("load", ()=>{setDate()})
+function saveMood() {
+    happinessValue = document.querySelector('input[name="h-value"]:checked').value;
+    angerValue = document.querySelector('input[name="a-value"]:checked').value;
+    let moods = [happinessValue, angerValue];
+    localStorage.setItem(`${date.getDate}.${date.getMonth}.${date.getFullYear}`, moods);
+    console.log(localStorage.getItem(`${date.getDate}.${date.getMonth}.${date.getFullYear}`))
+}
+
+function loadMood() {
+    
+    if (localStorage.getItem(`${date.getDate}.${date.getMonth}.${date.getFullYear}`)!=null) {
+        let moods = localStorage.getItem(`${date.getDate}.${date.getMonth}.${date.getFullYear}`)
+        document.querySelector(`input[name="h-value"]`, `input[value="${moods[0]}"]`).checked = true; 
+        document.querySelector(`input[name="a-value"]`, `input[value="${moods[1]}"]`).checked = false;
+    }
+}
+
+window.addEventListener("load", ()=>{loadMood(); setDate()});
+saveButton.addEventListener("click", ()=>{saveMood()})
